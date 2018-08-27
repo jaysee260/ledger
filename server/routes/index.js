202950097c2d10f565dd32a8d1882bda724807e6
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-08-26 22:50:38 -0400

    added comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
+/**
+ * Catalog of Application-Wide Routes
+ */
+
+ // Controllers
 const expenses = require("express").Router();
 
+// Attach Controller's Logic
 require("./expenses")(expenses);
 
+// Index of Routes
 const Routes = (router) => {
 
   router.use("/expenses", expenses);
